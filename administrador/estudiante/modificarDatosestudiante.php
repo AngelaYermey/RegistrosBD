69b8276c11ad238a4fb5d9046b45e0879b791143
AngelaYermey <carrionangela17@gmail.com>
2024-08-14 17:46:11 -0500

    ...

--- administrador/estudiante/modificarDatosestudiante.php
+++ administrador/estudiante/modificarDatosestudiante.php
@@ -17,12 +17,9 @@ if ($validar == null || $validar == '') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/DiseñoCuenta.css">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-    <script src="https://kit.fontawesome.com/5ef4b61a8f.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../../css/formModificar.css">
-    <title>Modificar</title>
+    <title>ROEH: modificar estudiante</title>
 </head>
 
 <?php
@@ -62,77 +59,83 @@ $resultCentros = $conn->query($query);
         include("../../menuFooter/encabezadoA.html");
         ?>
     </header>
-    <form method="POST" class="custom-form-style">
-        <br>
-        <h3>Modificar Estudiante</h3><br>
-        <div class="col-md-12">
-            <?php include "actualizar_estudiante.php"; ?>
-            <label for="nom" class="form-label">Nombre</label>
-            <input type="text" id="nom" class="form-control" name="nom" pattern="[A-Za-zÁáÉéÍíÓóÚúÜüÑñ]+" title="Solo se permiten letras mayúsculas y minúsculas" value="<?php echo $datos->nombre; ?>" required>
-        </div>
-        <div class="col-md-12">
-            <label for="ape" class="form-label">Apellido</label>
-            <input type="text" id="ape" class="form-control" name="ape" pattern="[A-Za-zÁáÉéÍíÓóÚúÜüÑñ]+" title="Solo se permiten letras mayúsculas y minúsculas" value="<?php echo $datos->apellido; ?>" required>
-        </div>
-        <div class="col-12">
-            <label for="cedula" class="form-label">Cédula</label>
-            <input type="text" id="cedula" name="nueva_cedula" class="form-control" pattern="(\d{1,2}|PE|E|N|\d{1,2}AV|\d{1,2}PI)-\d{1,4}-\d{1,5}" title="Formato: XX-XXXX-XXXXX" value="<?php echo $datos->cedula_estudiante; ?>" required>
-            <!-- Campo oculto para almacenar la cédula antigua por si necesita modificar este campo-->
-            <input type="hidden" name="cedAntigua" value="<?php echo $datos->cedula_estudiante; ?>">
-        </div>
-        <div class="col-12">
-            <label for="correo" class="form-label">Correo Institucional</label>
-            <input type="email" id="correo" name="correo" class="form-control" title="Formato: @utp.ac.pa " pattern="^[\w.%+-]+@([a-zA-Z0-9-]+\.)*utp\.ac\.pa$" value="<?php echo $datos->email; ?>" required>
-        </div>
+    <section>
+        <div class="contentbx">
+            <div class="form">
+                <h2>Modificar Estudiante</h2><br>
+                <form method="POST" class="custom-form-style">
+                    <div class="form-floating mb-3">
+                        <?php include "actualizar_estudiante.php"; ?>
+                        <label for="nom" class="form-label">Nombre</label>
+                        <input type="text" id="nom" class="form-control" name="nom" pattern="[A-Za-zÁáÉéÍíÓóÚúÜüÑñ]+" title="Solo se permiten letras mayúsculas y minúsculas" value="<?php echo $datos->nombre; ?>" required>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <label for="ape" class="form-label">Apellido</label>
+                        <input type="text" id="ape" class="form-control" name="ape" pattern="[A-Za-zÁáÉéÍíÓóÚúÜüÑñ]+" title="Solo se permiten letras mayúsculas y minúsculas" value="<?php echo $datos->apellido; ?>" required>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <label for="cedula" class="form-label">Cédula</label>
+                        <input type="text" id="cedula" name="nueva_cedula" class="form-control" pattern="(\d{1,2}|PE|E|N|\d{1,2}AV|\d{1,2}PI)-\d{1,4}-\d{1,5}" title="Formato: XX-XXXX-XXXXX" value="<?php echo $datos->cedula_estudiante; ?>" required>
+                        <!-- Campo oculto para almacenar la cédula antigua por si necesita modificar este campo-->
+                        <input type="hidden" name="cedAntigua" value="<?php echo $datos->cedula_estudiante; ?>">
+                    </div>
+                    <div class="form-floating mb-3">
+                        <label for="correo" class="form-label">Correo Institucional</label>
+                        <input type="email" id="correo" name="correo" class="form-control" title="Formato: @utp.ac.pa " pattern="^[\w.%+-]+@([a-zA-Z0-9-]+\.)*utp\.ac\.pa$" value="<?php echo $datos->email; ?>" required>
+                    </div>
 
-        <div class="col-12">
-            <label for="facultad" class="form-label">Facultad</label>
-            <input type="text" id="facultad" name="facultad" class="form-control" title="Por favor, ingrese su facultad." value="<?php echo $datos->facultad; ?>" required>
-        </div>
-        <div class="col-12">
-            <label for="carrera" class="form-label">Carrera</label>
-            <input type="text" id="carrera" name="carrera" class="form-control" title="Por favor, ingrese su carrera." value="<?php echo $datos->carrera; ?>" required>
-        </div>
+                    <div class="form-floating mb-3">
+                        <label for="facultad" class="form-label">Facultad</label>
+                        <input type="text" id="facultad" name="facultad" class="form-control" title="Por favor, ingrese su facultad." value="<?php echo $datos->facultad; ?>" required>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <label for="carrera" class="form-label">Carrera</label>
+                        <input type="text" id="carrera" name="carrera" class="form-control" title="Por favor, ingrese su carrera." value="<?php echo $datos->carrera; ?>" required>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <label for="año" class="form-label">Año:</label>
+                        <input type="text" id="año" name="año" pattern="[1-5]" class="form-control" value="<?php echo $datos->año; ?>" required>
+                    </div>
 
-        <div class="col-12">
-            <label for="año">Año:</label>
-            <input type="text" id="año" name="año" pattern="[1-5]" value="<?php echo $datos->año; ?>" title="[1-5]" required>
-        </div>
+                    <div class="form-floating mb-3">
+                        <label for="cr" class="form-label">Centro Regional:</label>
+                        <select class="form-control" aria-label="Disabled select example" required>
+                            <option value="">Escoger opción...</option>
+                            <?php while ($row = $resultCentros->fetch_assoc()) : ?>
+                                <option value="<?php echo $row['id_centroRegional']; ?>" <?php echo $datos->id_centroRegional == $row['id_centroRegional'] ? "selected" : ""; ?>>
+                                    <?php echo $row['nombre_centro']; ?>
+                                </option>
+                            <?php endwhile; ?>
+                        </select>
+                    </div>
 
-        <div class="col-12">
-            <label for="cr">Centro Regional:</label>
-            <select id="cr" name="id_centroRegional" class="form-select" required>
-                <option value="">Escoger opción...</option>
-                <?php while ($row = $resultCentros->fetch_assoc()) : ?>
-                    <option value="<?php echo $row['id_centroRegional']; ?>" <?php echo $datos->id_centroRegional == $row['id_centroRegional'] ? "selected" : ""; ?>><?php echo $row['nombre_centro']; ?></option>
-                <?php endwhile; ?>
-            </select>
-        </div>
-        <div class="col-12">
-            <label for="numAula">N° Aula</label>
-            <input type="text" id="aula" name="aula" title="Por favor, ingrese el número del aula." value="<?php echo $datos->numero_aula; ?>" required><br>
-        </div>
-        <div class="col-12">
-            <label for="pass" class="form-label">Contraseña</label>
-            <input type="text" id="pass" name="pass" class="form-control" value="<?php echo $datos->contraseña; ?>" required>
-        </div>
-
-        <div class="row p-4 ">
-            <div class="col-6">
-                <center><button type="submit" class="btn btn-success" name="btnModificar" value="ok">Modificar</button></center>
+                    <div class="form-floating mb-3">
+                        <label for="numAula" class="form-label">N° Aula</label>
+                        <input type="text" id="aula" name="aula" class="form-control" title="Por favor, ingrese el número del aula." value="<?php echo $datos->numero_aula; ?>" required>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <label for="pass" class="form-label">Contraseña</label>
+                        <input type="text" id="pass" name="pass" class="form-control" value="<?php echo $datos->contraseña; ?>" required>
+                    </div>
+                    <div class="inputbx2">
+                        <div class="row p-4 ">
+                            <div class="col-6">
+                                <center><button type="submit" class="btn btn-success" name="btnModificar" value="ok">Modificar</button></center>
+                            </div>
+                            <div class="col-6">
+                                <center><a href="tablaEstudiante.php" name="Cancelar" class="btn btn-secondary">Cancelar</a></center>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div class="col-6">
-                <center><a href="tablaEstudiante.php" name="Cancelar" class="btn btn-secondary">Cancelar</a></center>
-            </div>
-        </div>
-
-    </form>
+    </section>
     <footer class="footer">
         <?php
         include("../../menuFooter/footerA.html");
         ?>
     </footer>
-    
+
 </body>
 
 
